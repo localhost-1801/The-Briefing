b6db079bbcb93aa3de672ae69e032bce18279f79
Warren Mui <warrenmui3225@gmail.com>
2018-03-15 14:57:38 -0500

    front end form field

--- analyzers/toneAnalyzer.js
+++ analyzers/toneAnalyzer.js
@@ -6,26 +6,27 @@ var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 const url = 'http://www.bbc.com/news/world-us-canada-43402077'
 //const url = 'http://www.foxnews.com/world/2018/03/14/23-russian-diplomats-to-be-expelled-from-britain-amid-probe-into-ex-spys-poisoning.html';
 
+masterArticleScrapper(url)
 
 //const object = await masterArticleScrapper(url);
-const test = async () => {
-    const object = await masterArticleScrapper(url)
-    return object
-}
-//console.log('LOGsdfsdfsdfsfsd LOG', test());
-let testresults = test();
-console.log(testresults)
-//console.log('LOG LOG LOG', test().then(result => result));
+// const test = async () => {
+//     const object = await masterArticleScrapper(url)
+//     return object
+// }
+// //console.log('LOGsdfsdfsdfsfsd LOG', test());
+// let testresults = test();
+// console.log(testresults)
+// //console.log('LOG LOG LOG', test().then(result => result));
 
-// const wrapper = 
+// // const wrapper = 
 
-var toneAnalyzer = new ToneAnalyzerV3({
-    "url": "https://gateway.watsonplatform.net/tone-analyzer/api",
-    "username": "c4532afd-b6f2-4d58-a4bd-a4d67f9683ac   ",
-    "password": "pNCqrCNSQf5b",
-    version: '2017-09-21',
-    sentences: false
-});
+// var toneAnalyzer = new ToneAnalyzerV3({
+//     "url": "https://gateway.watsonplatform.net/tone-analyzer/api",
+//     "username": "c4532afd-b6f2-4d58-a4bd-a4d67f9683ac   ",
+//     "password": "pNCqrCNSQf5b",
+//     version: '2017-09-21',
+//     sentences: false
+// });
 
 // toneAnalyzer.tone(
 
