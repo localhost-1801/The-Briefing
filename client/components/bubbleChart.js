29524a7dd17f29f1ea6a1db70bc4f55685cee2a2
Thomas Lawless <tlawless94@gmail.com>
2018-03-22 15:55:39 -0500

    rendering something

--- client/components/bubbleChart.js
+++ client/components/bubbleChart.js
@@ -1,83 +1,87 @@
-import React            from 'react';
-import ReactBubbleChart from 'react-bubble-chart';
-// import Actions          from '../Actions';
+import React from 'react';
+import { connect } from 'react-redux';
+import { ResponsiveBubble } from '@nivo/circle-packing';
 
-var colorLegend = [
-  //reds from dark to light
-  {color: "#67000d", text: 'Negative', textColor: "#ffffff"}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
-  //neutral grey
-  {color: "#f0f0f0", text: 'Neutral'},
-  // blues from light to dark
-  "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Positive', textColor: "#ffffff"}
-];
-
-var tooltipProps = [{
-  css: 'symbol',
-  prop: '_id'
-}, {
-  css: 'value',
-  prop: 'value',
-  display: 'Last Value'
-}, {
-  css: 'change',
-  prop: 'colorValue',
-  display: 'Change'
-}];
 
 class BubbleChart extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      allArticles: false,
-    }
-  }
 
-  parseData(data){
-
-  }
-
-  parseDataMultiple(data){
-
-  }
-
-  render () {
-    let data = [];
-    if(this.state.allArticles){
-      data = parseDataMultiple(this.props.relatedArticles)
-    } else {
-      data = parseData(this.props.singleArticle)
-    }
-    var data = this.props.data.map(d => ({
-      _id: d._id,
-      value: d.value,
-      colorValue: d.sentiment,
-      selected: d.selected
-    }));
-
-    return <ReactBubbleChart
-      className="my-cool-chart"
-      colorLegend={colorLegend}
-      data={data}
-      selectedColor="#737373"
-      selectedTextColor="#d9d9d9"
-      fixedDomain={{min: -1, max: 1}}
-      onClick={Actions.doStuff.bind(Actions)}
-      legend={true}
-      legendSpacing={0}
-      tooltip={true}
-      tooltipProps={tooltipProps}
-      tooltipFunc={tooltipFunc}
-    />;
+  render(){
+    return(
+      <div className='bubbleWrapper'>
+      <ResponsiveBubble
+        root={
+          {
+            "name": "pie",
+            "color": "hsl(284, 70%, 50%)",
+            "children": [
+              {
+                "name": "outline",
+                "color": "hsl(335, 70%, 50%)",
+                "loc": 156025
+              },
+              {
+                "name": "slices",
+                "color": "hsl(86, 70%, 50%)",
+                "loc": 64359
+              },
+              {
+                "name": "bbox",
+                "color": "hsl(64, 70%, 50%)",
+                "loc": 97842
+              }
+            ]
+          }
+        }
+        margin={{
+            "top": 20,
+            "right": 20,
+            "bottom": 20,
+            "left": 20
+        }}
+        identity="name"
+        value="loc"
+        colors="nivo"
+        colorBy="name"
+        leavesOnly={true}
+        padding={6}
+        labelTextColor="inherit:darker(0.8)"
+        borderWidth={2}
+        defs={[
+            {
+                "id": "lines",
+                "type": "patternLines",
+                "background": "none",
+                "color": "inherit",
+                "rotation": -45,
+                "lineWidth": 5,
+                "spacing": 8
+            }
+        ]}
+        fill={[
+            {
+                "match": {
+                    "depth": 1
+                },
+                "id": "lines"
+            }
+        ]}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={12}
+    />
+  </div>
+    )
   }
 }
 
-const mapState = ({ singleArticle }) => ({ singleArticle })
+const mapState = ({ singleArticle, relatedArticles }) => ({ singleArticle, relatedArticles })
 const mapDispatch = (dispatch) => {
-  return {
-    loadData(url) {
-      dispatch(fetchArticleData(url))
+    return {
+        loadData(url) {
+            dispatch(fetchArticleData(url))
+            dispatch(fetchRelatedArticles(url))
+        }
     }
-  }
 }
 
-module.exports = BubbleChart;
+export default connect (mapState, mapDispatch)(BubbleChart)
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Route, Switch } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, ArticleAnalyzer, OverallSentimentAnalysis, LandingPage, singleArticleData, BarChart} from './components'
+import {Login, Signup, BubbleChart, UserHome, ArticleAnalyzer, OverallSentimentAnalysis, LandingPage, singleArticleData} from './components'
 import {me} from './store'
 
 /**
@@ -20,7 +20,7 @@ class Routes extends Component {
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route exact path="/" component={LandingPage} />
-        <Route exact path="/test" component={BarChart} />
+        <Route exact path="/test" component={BubbleChart} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/singleArticleData" component={singleArticleData} />
