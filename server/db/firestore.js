333ea952304b80877ea952543b2742987ee07fab
Thomas Lawless <tlawless94@gmail.com>
2018-03-23 16:36:47 -0500

    remove if block just JSON.parse

--- server/db/firestore.js
+++ server/db/firestore.js
@@ -1,10 +1,7 @@
 const admin = require('firebase-admin')
 let serviceAccount = '';
 try {
-  serviceAccount = require('../../googleKey.json')
-  if (serviceAccount.length === 0){
-    serviceAccount = JSON.parse(process.env.FIRESTORE)
-  }
+  serviceAccount = JSON.parse(process.env.FIRESTORE)
 }
 catch (err){
   console.log('error:', err)
