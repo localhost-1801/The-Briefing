1e7ac03c7cd0964c90eee7f202ebe9c24e2603bf
Thomas Lawless <tlawless94@gmail.com>
2018-03-26 14:25:03 -0500

    bug with positive negative

--- client/components/overallSentimentAnalysisWithProps.js
+++ client/components/overallSentimentAnalysisWithProps.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { VictoryPie, VictoryAnimation, VictoryLabel } from 'victory';
 import { connect } from 'react-redux'
+import ReactLoading from 'react-loading'
 import { fetchArticleData, makeArticle } from '../store/singleArticle'
 
 // https://formidable.com/open-source/victory/gallery/animating-circular-progress-bar/
@@ -9,46 +10,65 @@ class OverallSentimentAnalysisWithProps extends Component {
     constructor() {
         super();
         this.state = {
-            bool: true
+            aggregate: true,
         }
-        // this.state = {
-        //     percent: 0, data: this.getData(0) //percentage should always start at zero
-        // };
+        this.parseData = this.parseData.bind(this)
+        this.parseDataMultiple = this.parseDataMultiple.bind(this)
     }
 
-    // componentDidMount() {
-    //     this.props.loadData();
-    //     this.setState({ bool: !this.state.bool })
-    // }
+    handleClick(){
+      this.setState({
+        aggregate: !this.state.aggregate
+      })
+    }
 
-    //   componentDidMount() {
-    //     let percent = 70 // this would feed from Watson
-    //       this.setState({
-    //         percent, data: this.getData(percent)
-    //       });
-    //   }
+    parseData(data){
+      let score = data.nlu.sentiment.document.score
+      let percent = Math.abs(Math.floor(score * 100)) || 50
+      let sum = Math.floor(score * 100) > 0 ?
+      return [{x:1, y: percent}, {x: 2, y: 100 - percent}, {isPositive: }]
+    }
+
+    parseDataMultiple(dataArr){
+      let avgPercent = 0;
+      let iterated = false;
+      dataArr.forEach( (dataObj, index) => {
+        let score = dataObj.nlu.sentiment.document.score;
+        positiveNegative += score;
+        let denom = iterated ? 2 : 1;
+        avgPercent += (Math.abs(Math.floor(score * 100)) || 0) / denom;
+      })
+      let newArr = [{x:1, y: avgPercent }, {x:2, y: 100 - avgPercent}];
+      return newArr
+    }
 
     getData(percent) {
         return [{ x: 1, y: percent }, { x: 2, y: 100 - percent }];
     }
 
     render() {
-        let isPositiveInt = (this.props.singleArticle.nlu.sentiment.document.score * 100) > 0 ? true : false;
-        let data;
-        // if (this.state.bool) {
+        if(this.props.relatedArticles.length === 0){
+          return <ReactLoading type={'spin'} color={'#708090'} height='100px' width='100px' />
+        }
+        let data = this.state.aggregate ? this.parseDataMultiple(this.props.relatedArticles) : this.parseData(this.props.singleArticle)
+        let positive =
+                // if (this.state.bool) {
             // data = { percent: 0, data: [{ x: 1, y: 0 }, { x: 2, y: 100 }] }
         // } else {
-            data = {
-                percent: Math.abs(Math.floor(this.props.singleArticle.nlu.sentiment.document.score  * 100)) || 50,
-                data: this.getData(Math.abs(Math.floor(this.props.singleArticle.nlu.sentiment.document.score * 100))) || 50
-            // }
-        }
+        //     data = {
+        //         percent: Math.abs(Math.floor(this.props.singleArticle.nlu.sentiment.document.score  * 100)) || 50,
+        //         data: this.getData(Math.abs(Math.floor(this.props.singleArticle.nlu.sentiment.document.score * 100))) || 50
+        //     // }
+        // }
         //[{x:1, y:79}, {x:2, y:21}]
 
         //    [{x:1, y:0}, {x:2, y:100}]
         // {percent: 70, data: [{x:1, y:70}, {x:2, y:30}]}
         return (
             <div className="chartBackground">
+              <button onClick={this.handleClick}>
+                {this.state.aggregate ? 'Your Article' : 'Aggregate'}
+              </button>
                 <svg viewBox="0 0 400 400" width="100%" height="100%">
                     <VictoryPie // can update the height and width on the line above
                         standalone={false}
@@ -61,7 +81,7 @@ class OverallSentimentAnalysisWithProps extends Component {
                         style={{
                             data: {
                                 fill: (d) => {
-                                    const color = isPositiveInt ? 'green' : 'red'; // might want to reformat this to say if 'positive' from watson ? 'green' : 'red'
+                                    const color = positive ? 'green' : 'red'; // might want to reformat this to say if 'positive' from watson ? 'green' : 'red'
                                     return d.x === 1 ? color : 'gray';
                                 }
                             }
@@ -74,7 +94,7 @@ class OverallSentimentAnalysisWithProps extends Component {
                                     textAnchor="middle" verticalAnchor="middle"
                                     x={200} y={200}
                                     text={`${Math.round(data.percent)}% ${this.props.singleArticle.nlu.sentiment.document.label}`}
-                                   // ${isPositiveInt ? 'Positive' : 'Negative'}`} // should upate 'Positive' with the info from Watson
+                                   // ${positive ? 'Positive' : 'Negative'}`}
                                     style={{ fontSize: 45 }}
                                 />
                             );
@@ -87,7 +107,7 @@ class OverallSentimentAnalysisWithProps extends Component {
 }
 
 
-const mapState = ({ singleArticle }) => ({ singleArticle })
+const mapState = ({ singleArticle, relatedArticles }) => ({ singleArticle, relatedArticles })
 const mapDispatch = (dispatch) => {
     return {
         loadData(url) {
